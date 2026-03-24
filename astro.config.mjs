32bbf70f136d4ba045b268cf4f304372a357a85f
Prathamesh Sakhadeo <sakhadeo.p@gmail.com>
2026-03-24 10:51:59 +0530

    css consolidated and tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elamrit.com',
   trailingSlash: 'never',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
